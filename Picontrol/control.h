d381c65c1610107503a6805ba54f0c8d6cc5a161
Tyler Martin <tyler.martin@oit.edu>
2012-12-07 19:05:14 -0800

    added appropriate function headers

--- Picontrol/control.h
+++ Picontrol/control.h
@@ -2,6 +2,17 @@
 #define _CONTROL_H_
 
 void command( int cmd);
-void initControl();
+void forward();
+void backward();
+void turnRight();
+void turnLeft();
+void panRight();
+void panLeft();
+void tiltUp();
+void tiltDown();
+void LEDOn();
+void LEDOff();
+void stopTread();
+void stopCam();
 
 #endif
